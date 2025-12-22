8d8a19be7a54f8062f005262fb6904d1a676b41e
MahmoudMohamedAbdelgelil125 <seigkassabahmedkamal@gmail.com>
2025-12-22 11:06:07 +0200

    feat: initialize Celery app for task management in Django

---
+++ src/watchtower_ce/celery.py
@@ -0,0 +1,23 @@
+import os
+from celery import Celery
+
+"""
+Set default Django settings module
+"""
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "watchtower_ce.settings")
+
+"""
+Create Celery app instance
+"""
+app = Celery("watchtower_ce")
+
+""" 
+Load settings from Django settings.py, prefix CELERY_
+
+"""
+app.config_from_object("django.conf:settings", namespace="CELERY")
+
+"""
+Autodiscover tasks from installed apps
+"""
+app.autodiscover_tasks()
