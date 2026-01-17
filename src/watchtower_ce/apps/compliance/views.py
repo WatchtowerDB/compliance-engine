6166bcf8fe96a4f5e8bb39f10f4b66eccb2cd231
MahmoudMohamedAbdelgelil125 <seigkassabahmedkamal@gmail.com>
2026-01-17 16:25:38 +0200

    fix(compliance): remove DRF-incompatible type annotations from viewsets

--- src/watchtower_ce/apps/compliance/views.py
+++ src/watchtower_ce/apps/compliance/views.py
@@ -1,11 +1,9 @@
 from django.db.models import QuerySet
 from rest_framework import viewsets
 from rest_framework.response import Response
-from rest_framework.serializers import Serializer
 from .tasks import (
     schedule_sql_assertion_pipeline,
 )
-
 
 from . import models, serializers
 
@@ -16,7 +14,7 @@ class ComplianceFrameworkViewSet(viewsets.ReadOnlyModelViewSet):
     """
 
     queryset: QuerySet = models.ComplianceFramework.objects.all()
-    serializer_class: type[Serializer] = serializers.ComplianceFrameworkSerializer
+    serializer_class = serializers.ComplianceFrameworkSerializer
 
 
 class ClientDBViewSet(viewsets.ModelViewSet):
@@ -26,12 +24,12 @@ class ClientDBViewSet(viewsets.ModelViewSet):
     """
 
     queryset: QuerySet = models.ClientDB.objects.all()
-    serializer_class: type[Serializer] = serializers.ClientDBSerializer
+    serializer_class = serializers.ClientDBSerializer
 
 
 class ClientDBSchemaViewSet(viewsets.ModelViewSet):
     queryset: QuerySet = models.ClientDBSchema.objects.all()
-    serializer_class: type[Serializer] = serializers.ClientDBSchemaSerializer
+    serializer_class = serializers.ClientDBSchemaSerializer
 
     # Disallow updates/deletes
     def update(self, request, *args, **kwargs):
@@ -50,7 +48,7 @@ class ComplianceAssertionViewSet(viewsets.ReadOnlyModelViewSet):
     """
 
     queryset: QuerySet = models.ComplianceAssertion.objects.all()
-    serializer_class: type[Serializer] = serializers.ComplianceAssertionSerializer
+    serializer_class = serializers.ComplianceAssertionSerializer
 
 
 class ComplianceCheckViewSet(viewsets.ModelViewSet):
@@ -61,7 +59,7 @@ class ComplianceCheckViewSet(viewsets.ModelViewSet):
     """
 
     queryset: QuerySet = models.ComplianceCheck.objects.all()
-    serializer_class: serializers.ComplianceCheckSerializer
+    serializer_class = serializers.ComplianceCheckSerializer
     http_method_names: list[str] = ["get", "post", "head", "options"]
 
     def create(self, request, *args, **kwargs):
