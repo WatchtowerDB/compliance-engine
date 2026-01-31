76d32223137c359a072b967f6262fb0b89496a41
Sammany1 <ialsammany@gmail.com>
2026-01-31 06:51:36 +0200

    feat: enhance compliance models and filters with new fields and relationships

--- src/watchtower_ce/apps/compliance/filters.py
+++ src/watchtower_ce/apps/compliance/filters.py
@@ -5,12 +5,13 @@ from .models import ComplianceAssertion, ClientDBSchema
 class ComplianceAssertionFilter(filters.FilterSet):
     schema = filters.NumberFilter(field_name="schema__id")
     client_db = filters.NumberFilter(field_name="client_db__id")
-    compliance_framework = filters.NumberFilter(field_name="compliance_framework__id")
+    compilance_framework = filters.NumberFilter(field_name="compliance_framework__id")
     result = filters.BooleanFilter(field_name="result")
+    check = filters.NumberFilter(field_name="compliance_check__id")
 
     class Meta:
         model = ComplianceAssertion
-        fields = ["schema", "client_db", "compliance_framework", "result"]
+        fields = ["schema", "client_db", "compliance_framework", "result", "check"]
 
 
 class ClientDBSchemaFilter(filters.FilterSet):
