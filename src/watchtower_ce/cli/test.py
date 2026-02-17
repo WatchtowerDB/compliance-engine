25850b7a516ffb90bfe691151978bb7efc510858
A. Rahal <120186121+AbdelRahmanRahal@users.noreply.github.com>
2026-02-17 10:00:48 +0200

    fix: wrapped the debug log in an option
    It otherwise would get triggered in every other command

--- src/watchtower_ce/cli/test.py
+++ src/watchtower_ce/cli/test.py
@@ -10,8 +10,6 @@ from ..engine.test_suite.analysis_quality_evaluator import AnalysisQualityEvalua
 from ..engine.test_suite.evaluation_metrics import EvaluationMetrics
 from ..engine.test_suite.gold_standard import create_analysis_quality_test_dataset
 
-logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
-
 
 @click.command(
     "test",
@@ -26,16 +24,22 @@ logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
     help="Number of iterations to run the test suite. The results of all iterations are aggregated. Higher values give more accurate scores but take more time to run.",
     default=1,
 )
+@click.option("-d", "--debug", is_flag=True, help="Enable debug logging.")
 def test(
-    iterations: int,
-) -> None:  # TODO: WHEN MORE STANDARDS ARE IMPLEMENTED, THE TEST COMMAND SHOULD TAKE A "STANDARD" ARGUMENT
+    iterations: int, debug: bool
+) -> None:  # TODO: WHEN MORE STANDARDS ARE IMPLEMENTED, THE TEST COMMAND SHOULD TAKE A "STANDARD"
+    if debug:
+        logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
+
     print("Creating analysis quality test dataset...")
     test_cases = create_analysis_quality_test_dataset()
     print(f"Created {len(test_cases)} test cases\n")
 
     evaluator = AnalysisQualityEvaluator(test_cases)
 
-    print(settings.BASE_MODEL_PATH)
+    print(
+        settings.BASE_MODEL_PATH
+    )  # TODO: REMOVE THESE AFTER FIXING THE ISSUE (IF THERE IS ONE)
     print(settings.CHROMA_DIR)
     print(settings.EMBEDDING_MODEL_DIR)
 
