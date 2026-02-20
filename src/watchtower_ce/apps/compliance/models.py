d9bc8440199c8c9c6176197ee808f7f5534e0454
MahmoudMohamedAbdelgelil125 <seigkassabahmedkamal@gmail.com>
2026-02-20 22:36:29 +0200

    fix(schema): restore sql_definition field in ClientDBSchema

--- src/watchtower_ce/apps/compliance/models.py
+++ src/watchtower_ce/apps/compliance/models.py
@@ -32,6 +32,7 @@ class ClientDBSchema(models.Model):
 
     name = models.CharField(max_length=255, null=True, blank=True)
     description = models.TextField(blank=True, null=True)
+    sql_definition = models.TextField(null=True, blank=True)
 
     schema_json = models.JSONField(null=True, blank=True)
 
