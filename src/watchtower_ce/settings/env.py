66b64d06846847653928495249b057e71a261106
Youssef Fathy <32334265+0xzer0x@users.noreply.github.com>
2026-03-29 09:36:41 +0200

    refactor(settings): use database url instead of multiple env vars

--- src/watchtower_ce/settings/env.py
+++ src/watchtower_ce/settings/env.py
@@ -2,6 +2,7 @@ import os
 import typing as t
 from pathlib import Path
 
+import dj_database_url
 from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,25 +25,14 @@ SECRET_KEY: str = os.getenv(
     "django-dev-d6d=nm0@u_1+&f_go09c8w07-t8@z$wr*(wi(vn*$a9!bk=^o3",
 )
 
-_DB_CONFIG: dict[str, dict[str, str | Path]] = {
-    "DEV": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.getenv("DJANGO_DB_PATH", BASE_DIR / "db.sqlite3"),
-    },
-    "PROD": {
-        "ENGINE": "django.db.backends.postgresql",
-        "HOST": os.getenv("DJANGO_DB_HOST", "localhost"),
-        "PORT": os.getenv("DJANGO_DB_PORT", "5432"),
-        "NAME": os.getenv("DJANGO_DB_NAME", "watchtower_ce"),
-        "USER": os.getenv("DJANGO_DB_USER", ""),
-        "PASSWORD": os.getenv("DJANGO_DB_PASSWORD", ""),
-    },
-}
-
-DATABASES: dict[str, dict[str, str | Path]] = {
-    "default": _DB_CONFIG.get(
-        os.getenv("DJANGO_ENVIRONMENT", "").upper(), _DB_CONFIG["DEV"]
-    )
+# WARN: Database defaults to db.sqlite in nu_quran_api directory if DATABASE_URL is not set
+# Refer to https://pypi.org/project/dj-database-url/ for URL schemas for different databases
+DATABASES: dict[str, dj_database_url.DBConfig] = {
+    "default": dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+    ),
 }
 
 CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://redis:6379/0")
