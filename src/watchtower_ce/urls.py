d566bb2a95851aca59c9692e6228583430b84eb5
A. Rahal <120186121+AbdelRahmanRahal@users.noreply.github.com>
2026-05-04 21:08:02 +0300

    style: relative import

--- src/watchtower_ce/urls.py
+++ src/watchtower_ce/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import URLPattern, URLResolver, include, path
 from drf_spectacular.views import SpectacularSwaggerView
 
-from watchtower_ce.apps.users.views import (
+from .apps.users.views import (
     HybridTokenObtainPairView,
     HybridTokenRefreshView,
 )
