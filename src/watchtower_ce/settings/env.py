4848d909570cf2c32e83697f70725b595290d05a
A. Rahal <120186121+AbdelRahmanRahal@users.noreply.github.com>
2026-05-06 20:27:02 +0300

    refactor: cleaner method of resolving parents

--- src/watchtower_ce/settings/env.py
+++ src/watchtower_ce/settings/env.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import dj_database_url
 from dotenv import load_dotenv
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parents[1]
 
 load_dotenv(
     os.getenv(
